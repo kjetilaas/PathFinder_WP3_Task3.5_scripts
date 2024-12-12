42f742f061de81504833632c2e73cdbe19b8c536
kjetilaas <k.s.aas@geo.uio.no>
2024-12-12 10:19:03 +0100

    Add download scripts and plots

---
+++ download_data.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+indir=input_files/ #/div/no-backup/CORDEX/EUR-11/
+
+#Modify to relevant cordex file names
+cordex_GCMmodel=MPI-M-MPI-ESM-LR
+cordex_GCM_sim=r1i1p1
+cordex_RCM=SMHI-RCA4_v1a #CLMcom-CCLM4-8-17_v1, MPI-CSC-REMO2009_v1
+cordex_hist_model=${cordex_GCMmodel}_historical_${cordex_GCM_sim}_${cordex_RCM}
+
+for var in pr tas; do #pr tas
+    for scenario in historical rcp45 rcp85; do #historical rcp26 rcp45 rcp85
+        file=$indir/WGET_${var}_EUR-11_${cordex_GCMmodel}_${scenario}_${cordex_GCM_sim}_${cordex_RCM}.sh; 
+        echo $file       
+        if [ -f $file ]; then
+            echo "Downloaded $var $scenario"
+            bash $file -s 
+            mkdir -p $indir/$scenario/$var/
+            mv ${var}_EUR-11_${cordex_GCMmodel}_${scenario}_${cordex_GCM_sim}_${cordex_RCM}*.nc $indir/$scenario/$var/
+        else
+            echo "Failed to download $var $scenario"
+        fi
+        
+    done
+done      
