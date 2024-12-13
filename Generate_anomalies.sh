3c4fbc8be5ebb797f0f3a0bc63766d9b82abb4e9
kjetilaas <k.s.aas@geo.uio.no>
2024-12-13 09:46:11 +0100

    Fix bugs and expanded README

--- Generate_anomalies.sh
+++ Generate_anomalies.sh
@@ -6,8 +6,11 @@
 #The result is a single output file with anomalies (for states, like temperature) or scaling factors (for fluxes, like precipitation).
 #Program uses CDO (https://code.mpimet.mpg.de/projects/cdo)
 
-module purge
-module load CDO/1.9.8-foss-2019b #Load CDO module, this is a the version available on Betzy, check with "module avail CDO"
+# Check if running on qbo
+if [ "$HOSTNAME" = "cic-qbo.hpc.uio.no" ]; then
+    module purge
+    module load CDO/1.9.8-foss-2019b #Load CDO module, this is a the version available on Betzy, check with "module avail CDO"
+fi
 
 #Modify to relevant cordex file names
 cordex_GCMmodel=MPI-M-MPI-ESM-LR
@@ -20,24 +23,15 @@ cordex_hist_model=${cordex_GCMmodel}_historical_${cordex_GCM_sim}_${cordex_RCM}
 indir=input_files/ #/div/no-backup/CORDEX/EUR-11/
 outdir=output_files/ #/storage/no-backup-nac/PATHFINDER/EURO-CORDEX/
 
-#Define varialbe and scenario
-var=pr #pr, tas 
-scenario=rcp45 #rcp26 rcp45, rcp85
+#Define varialbe and scenario. Now defined below in loop.
+#var=pr #pr, tas 
+#scenario=rcp45 #rcp26 rcp45, rcp85
 
 anomaly_list="tas uas vas ps huss"
 scale_list="pr rsds rlds" 
 
 syear_baseline=1996 #NB: climatology calculation is currently partly hardcoded to 1996-2005.
 eyear_baseline=2015 #NB: climatology calculation is currently partly hardcoded to 1996-2005.
-
-#Define derived paths and base filenames
-infile_hist_basename=$indir/historical/${var}/${var}_EUR-11_${cordex_GCMmodel}_historical_${cordex_GCM_sim}_${cordex_RCM}_mon
-outfile_baseclimate=$outdir/${var}_EUR-11_${cordex_hist_model}_yearmonavg_$syear_baseline-$eyear_baseline.nc
-outfile_anomaly=${var}_EUR-11_${cordex_GCMmodel}_${scenario}_${cordex_GCM_sim}_${cordex_RCM}_anomalies_2005-2100.nc
-
-infile_scenario_path=$indir/$scenario/$var/
-infile_scenario_basename=${var}_EUR-11_${cordex_GCMmodel}_${scenario}_${cordex_GCM_sim}_${cordex_RCM}_mon
-
 
 ###Download data from https://esgf-data.dkrz.de/search/esgf-dkrz/
 #Search for "cordex", "historical"/"rcp85","mon", "tas"/"pr", "EUR-11", "HIRHAM5", "v3". 
@@ -48,23 +42,32 @@ infile_scenario_basename=${var}_EUR-11_${cordex_GCMmodel}_${scenario}_${cordex_G
 for var in pr tas; do #pr tas
     echo 'Variable: '$var
 
-    ###Generate basline climatology. Currently partly hardcoded 1996-2015 baseline period. 
-    cdo ymonavg -selyear,$syear_baseline/$eyear_baseline -mergetime ${infile_hist_basename}_199101-200012.nc ${infile_hist_basename}_200101-200512.nc \
-        $indir/${scenario}/$var/${var}_EUR-11_${cordex_GCMmodel}_${scenario}_${cordex_GCM_sim}_${cordex_RCM}_mon_200601-201012.nc \
-        -selyear,2011/2015 $indir/${scenario}/$var/${var}_EUR-11_${cordex_GCMmodel}_${scenario}_${cordex_GCM_sim}_${cordex_RCM}_mon_201101-202012.nc \
-        $outfile_baseclimate
-
-    #If variable is pr, set min value to 10^-10 to avoid division by zero
-    if [ $var == "pr" ]; then
-        cdo -setrtoc,0,1e-10,1e-10 $outfile_baseclimate $outfile_baseclimate.temp
-        mv $outfile_baseclimate.temp $outfile_baseclimate
-    fi
-
     for scenario in rcp26 rcp45 rcp85; do #historical rcp26 rcp45 rcp85
         echo 'Scenario: '$scenario
 
+        #Define derived paths and base filenames
+        infile_hist_basename=$indir/historical/${var}/${var}_EUR-11_${cordex_GCMmodel}_historical_${cordex_GCM_sim}_${cordex_RCM}_mon
+        outfile_baseclimate=$outdir/${var}_EUR-11_${cordex_hist_model}_yearmonavg_$syear_baseline-$eyear_baseline.nc
+        outfile_anomaly=${var}_EUR-11_${cordex_GCMmodel}_${scenario}_${cordex_GCM_sim}_${cordex_RCM}_anomalies_2005-2100.nc
+
+        infile_scenario_path=$indir/$scenario/$var/
+        infile_scenario_basename=${var}_EUR-11_${cordex_GCMmodel}_${scenario}_${cordex_GCM_sim}_${cordex_RCM}_mon
+
+        ###Generate basline climatology. Currently partly hardcoded 1996-2015 baseline period.
+        cdo ymonavg -selyear,$syear_baseline/$eyear_baseline -mergetime ${infile_hist_basename}_199101-200012.nc ${infile_hist_basename}_200101-200512.nc \
+            $indir/${scenario}/$var/${var}_EUR-11_${cordex_GCMmodel}_${scenario}_${cordex_GCM_sim}_${cordex_RCM}_mon_200601-201012.nc \
+            -selyear,2011/2015 $indir/${scenario}/$var/${var}_EUR-11_${cordex_GCMmodel}_${scenario}_${cordex_GCM_sim}_${cordex_RCM}_mon_201101-202012.nc \
+            $outfile_baseclimate
+
+        #If variable is pr, set min value to 10^-10 to avoid division by zero
+        if [ $var == "pr" ]; then
+            cdo -setrtoc,0,1e-10,1e-10 $outfile_baseclimate $outfile_baseclimate.temp
+            mv $outfile_baseclimate.temp $outfile_baseclimate
+        fi
+
+
         #Merge data and split by month (for calculating running mean)
-        kdir -p $outdir/$scenario/$var/
+        mkdir -p $outdir/$scenario/$var/
 
         #Repeat last 10 years with shifted time stap, to be able to run "runmean" until 2100
         infile_lastdecade_temp=$infile_scenario_path/${infile_scenario_basename}_209101-210012.nc
--- README.md
+++ README.md
@@ -1,3 +1,61 @@
 Scripts to generate anomaly forcing from Euro Cordex files, for use in PATHFINDER project
 
-Feel free to post an issue if you need help or have questions. 
+## Description
+
+This repository contains scripts to calculate scaling factors and anomalies from Euro-Cordex data. These can be used in combination with historical forcing files to produce future forcing.
+
+## Steps to Use
+
+1. **Clone the repository:**
+
+    ```sh
+    git clone https://github.com/kjetilaas/PathFinder_WP3_Task3.5_scripts.git
+    cd PathFinder_WP3_Task3.5_scripts/
+    ```
+
+2. **Download data:**
+
+    Use the `download_data.sh` script to download the necessary data files:
+
+    ```sh
+    ./download_data.sh
+    ```
+
+3. **Load necessary modules:**
+
+    Ensure you have the required modules loaded. For example, on CICERO machine "qbo":
+
+    ```sh
+    module purge
+    module load CDO/1.9.8-foss-2019b
+    ```
+
+4. **Modify relevant variables:**
+
+    Update the script variables to match your specific dataset and paths. For example, in `Generate_anomalies.sh`:
+
+    ```sh
+    cordex_GCMmodel=MPI-M-MPI-ESM-LR
+    cordex_GCM_sim=r1i1p1
+    cordex_RCM=SMHI-RCA4_v1a
+    indir=input_files/
+    outdir=output_files/
+    ```
+
+    The script currently processes `tas` and `pr` variables. If you need to process other variables or use a different model combination, you will need to update the dataset and generate new download scripts accordingly.
+
+5. **Generate anomalies and scaling factors:**
+
+    Run the `Generate_anomalies.sh` script to process the data and generate the output files:
+
+    ```sh
+    ./Generate_anomalies.sh
+    ```
+
+6. **Check the output:**
+
+    The generated anomalies and scaling factors will be saved in the specified output directory.
+
+## Additional Information
+
+For more details on the methodology and usage, refer to the comments within the scripts and the references provided in the script headers. Questions can be posted as issues, or via email. 
